a611b0666debef1534206927fc47d88193527c15
tsaikd <tsaikd@115ed908-7b2c-0410-ac89-f8bc6e172fcc>
2007-03-26 09:56:44 +0000

    git-svn-id: http://svn.tsaikd.org/tsaikd/KDBlog/trunk@17 115ed908-7b2c-0410-ac89-f8bc6e172fcc

--- php/strSafeHtml.php
+++ php/strSafeHtml.php
@@ -4,7 +4,25 @@ function strSafeHtml($text) {
 	$text = str_replace("\\\"", "\"", $text);
 	$text = str_replace("\\'", "'", $text);
 	$text = htmlentities($text);
+	$text = preg_replace_callback("/%u[0-9A-Za-z]{4}/", toUtf8, $text);
 	return $text;
+}
+
+function toUtf8($ar) {
+	foreach($ar as $val) {
+		$val = intval(substr($val, 2), 16);
+		if ($val < 0x7F) {        // 0000-007F
+			$c .= chr($val);
+		} else if ($val < 0x800) { // 0080-0800
+			$c .= chr(0xC0 | ($val / 64));
+			$c .= chr(0x80 | ($val % 64));
+		} else {                // 0800-FFFF
+			$c .= chr(0xE0 | (($val / 64) / 64));
+			$c .= chr(0x80 | (($val / 64) % 64));
+			$c .= chr(0x80 | ($val % 64));
+		}
+	}
+	return $c;
 }
 
 ?>
